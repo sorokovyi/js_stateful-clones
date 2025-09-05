9c278765df8ea7a5b787b5d8619847110a4b36fb
admin <admin@iMac.lan>
2025-09-05 21:33:21 +0200

    fixed task 2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,20 +11,25 @@ function transformStateWithClones(state, actions) {
   let currentState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        currentState[key] = action.extraData[key];
-      }
-    }
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          currentState[key] = action.extraData[key];
+        }
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      currentState = {};
+      case 'clear':
+        currentState = {};
+        break;
+
+      default:
+        break;
     }
 
     history.push({ ...currentState });
